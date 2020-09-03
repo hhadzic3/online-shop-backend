a1f481fc0a5c482ca7c22a4adfe23c8302e6cb5f
hhadzic3 <hhadzic3@etf.unsa.ba>
2020-09-03 10:02:18 +0200

    Formatting vol1

--- api/categories.js
+++ api/categories.js
@@ -2,17 +2,29 @@ var express = require('express');
 var router = express.Router();
 const db = require('../db/db');
 
-router.get('/', (req, res) => db.categories.findAll().then(categories => res.json(categories)));
+router.get('/', (req, res) => {
+    db.categories.findAll().then( 
+        categories => 
+            res.json(categories)
+    )
+});
 
-router.get('/:id', (req, res) => db.categories.findOne({
-    where: {   id: req.params.id }}).then( data => { res.send(data) })   
-);
+router.get('/:id', (req, res) => {
+    db.categories.findOne({
+        where: { id: req.params.id }
+    }).then( data => { 
+        res.send(data)
+    })   
+});
 
 // DELETE
-router.delete('/:id' , (req, res) => db.categories.destroy({
-    where: {   id: req.params.id     }
- }).then( () => { res.json({ status : 'Deleted!'}) })  
-);
+router.delete('/:id' , (req, res) => {
+    db.categories.destroy({
+    where: { id: req.params.id }
+    }).then( () => {
+        res.json({ status : 'Deleted!'}) 
+    })  
+});
 
 // POST
 router.post('/' , function(req, res)  {
--- api/order_details.js
+++ api/order_details.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 const db = require('../db/db');
 
-router.get('/', (req, res) => db.order_details.findAll().then(order_details => res.json(order_details)));
+router.get('/', (req, res) => { 
+    db.order_details.findAll().then(order_details => res.json(order_details))
+});
 
 router.get('/:id', (req, res) => db.order_details.findOne({
     where: {   id: req.params.id }}).then( data => { res.send(data) })   
