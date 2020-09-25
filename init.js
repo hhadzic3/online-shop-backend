d926b300daeda1d651750185d6f600b6aa9b97fc
hhadzic3 <hhadzic3@etf.unsa.ba>
2020-09-25 16:13:16 +0200

    All finished

--- init.js
+++ init.js
@@ -20,8 +20,8 @@ function dataInit() {
     ];
 
     const ordersPromiseList = [
-        db.orders.create({id: 1,customer_id:1,ammount:569,shipping_address: 'Titova 99',order_address: 'Bakije sokak 33', order_email: "Hhamo@gmail.com",order_date: "28.04.2020",  order_status: 'ordered' , payment_method:'handover'}),
-        db.orders.create({id: 2,customer_id:1,ammount:569,shipping_address: 'Titova 99',order_address: 'Bakije sokak 33', order_email: "Hhamo@gmail.com",order_date: "28.04.2020",  order_status: 'ordered' , payment_method:'handover'}),
+       /* db.orders.create({id: 1,customer_id:1,ammount:569,shipping_address: 'Titova 99',order_address: 'Bakije sokak 33', order_email: "Hhamo@gmail.com",order_date: "28.04.2020",  order_status: 'ordered' , payment_method:'handover'}),
+        db.orders.create({id: 2,customer_id:1,ammount:569,shipping_address: 'Titova 99',order_address: 'Bakije sokak 33', order_email: "Hhamo@gmail.com",order_date: "28.04.2020",  order_status: 'ordered' , payment_method:'handover'}),*/
     ];
 
     const categoriesPromiseListe = [
@@ -31,6 +31,8 @@ function dataInit() {
         db.categories.create({id:4 , name: 'Accesories' , description: 'primary'}),
         db.categories.create({id:5 , name: 'Home' , description: 'primary'}),
         db.categories.create({id:6 , name: 'Electronics' , description: 'primary'}),
+        db.categories.create({id:20 , name: 'Vehicle' , description: 'primary'}),
+        db.categories.create({id:21 , name: 'Car' , description: 'subcategory Vehicle'}),
         db.categories.create({id:7 , name: 'Shoes' , description: 'subcategory Male Female Fashion'}),
         db.categories.create({id:8 , name: 'Sportware' , description: 'subcategory Male Female Fashion'}),
         db.categories.create({id:9 , name: 'Clothes' , description: 'subcategory Male Female Fashion'}),
@@ -51,71 +53,55 @@ function dataInit() {
         db.products.create({id: 2,seller_id:2 ,name: 'Iphone 11', price: 1009 , weight: 1 , description: 'The Nike Epic React Flyknit foam cushioning is responsive yet light-weight, durable yet soft. This creates a sensation that not only enhances the feeling of moving forward, but makes running feel fun, too.',label:"classic", stock: 1 }),
         db.products.create({id: 3,seller_id:2 ,name: 'HP laptop Probook', price: 2009 , weight: 1 , description: 'The Nike Epic React Flyknit foam cushioning is responsive yet light-weight, durable yet soft. This creates a sensation that not only enhances the feeling of moving forward, but makes running feel fun, too.',label:"last_chance", stock: 1 }),
         db.products.create({id: 4,seller_id:2 ,name: 'Macbook PRO laptop Apple', price: 2009 , weight: 1 , description: 'The Nike Epic React Flyknit foam cushioning is responsive yet light-weight, durable yet soft. This creates a sensation that not only enhances the feeling of moving forward, but makes running feel fun, too.',label:"last_chance", stock: 1 }),
-        db.products.create({id: 5,seller_id:2 ,name: 'Macbook AIR laptop Apple', price: 2009 , weight: 1 , description: 'The Nike Epic React Flyknit foam cushioning is responsive yet light-weight, durable yet soft. This creates a sensation that not only enhances the feeling of moving forward, but makes running feel fun, too.',label:"last_chance", stock: 1 }),
+        db.products.create({id: 5,seller_id:2 ,name: 'Mac Apple Computer', price: 2009 , weight: 1 , description: 'The Nike Epic React Flyknit foam cushioning is responsive yet light-weight, durable yet soft. This creates a sensation that not only enhances the feeling of moving forward, but makes running feel fun, too.',label:"last_chance", stock: 1 }),
         db.products.create({id: 6,seller_id:2 ,name: 'Shirt Hugo Boss', price: 59 , weight: 1 , description: 'The Nike Epic React Flyknit foam cushioning is responsive yet light-weight, durable yet soft. This creates a sensation that not only enhances the feeling of moving forward, but makes running feel fun, too.',label:"top_rated", stock: 1 }),
-        db.products.create({id: 7,seller_id:2 ,name: 'Jacket LV', price: 59 , weight: 1 , description: 'The Nike Epic React Flyknit foam cushioning is responsive yet light-weight, durable yet soft. This creates a sensation that not only enhances the feeling of moving forward, but makes running feel fun, too.',label:"top_rated", stock: 1 }),
-        db.products.create({id: 8,seller_id:2 ,name: 'Bag Gucci', price: 59 , weight: 1 , description: 'The Nike Epic React Flyknit foam cushioning is responsive yet light-weight, durable yet soft. This creates a sensation that not only enhances the feeling of moving forward, but makes running feel fun, too.',label:"top_rated", stock: 1 }),
-        db.products.create({id: 9,seller_id:2 ,name: 'Shoes Addidas', price: 109 , weight: 1 , description: 'The Nike Epic React Flyknit foam cushioning is responsive yet light-weight, durable yet soft. This creates a sensation that not only enhances the feeling of moving forward, but makes running feel fun, too.',label:"new_arrival", stock: 1 }),
-        db.products.create({id: 10,seller_id:2 ,name: 'Shoes Puma', price: 109 , weight: 1 , description: 'The Nike Epic React Flyknit foam cushioning is responsive yet light-weight, durable yet soft. This creates a sensation that not only enhances the feeling of moving forward, but makes running feel fun, too.',label:"new_arrival", stock: 1 }),
+       db.products.create({id: 7,seller_id:2 ,name: 'Barbecue', price: 270 , weight: 1 , description: 'The Nike Epic React Flyknit foam cushioning is responsive yet light-weight, durable yet soft. This creates a sensation that not only enhances the feeling of moving forward, but makes running feel fun, too.',label:"top_rated", stock: 1 }),
+        db.products.create({id: 8,seller_id:2 ,name: 'Ipad Pro Apple', price: 590 , weight: 1 , description: 'The Nike Epic React Flyknit foam cushioning is responsive yet light-weight, durable yet soft. This creates a sensation that not only enhances the feeling of moving forward, but makes running feel fun, too.',label:"top_rated", stock: 1 }),
+        db.products.create({id: 9,seller_id:2 ,name: 'Tesla electric car', price: 109000 , weight: 100 , description: 'The Nike Epic React Flyknit foam cushioning is responsive yet light-weight, durable yet soft. This creates a sensation that not only enhances the feeling of moving forward, but makes running feel fun, too.',label:"new_arrival", stock: 1 }),
+        db.products.create({id: 10,seller_id:2 ,name: 'Bad for kids', price: 1090 , weight: 25 , description: 'The Nike Epic React Flyknit foam cushioning is responsive yet light-weight, durable yet soft. This creates a sensation that not only enhances the feeling of moving forward, but makes running feel fun, too.',label:"new_arrival", stock: 1 }),
         db.products.create({id: 11,seller_id:2 ,name: 'Shoes Nike', price: 109 , weight: 1 , description: 'The Nike Epic React Flyknit foam cushioning is responsive yet light-weight, durable yet soft. This creates a sensation that not only enhances the feeling of moving forward, but makes running feel fun, too.',label:"new_arrival", stock: 1 }),
         db.products.create({id: 12,seller_id:2 ,name: 'Bag Furla', price: 400 , weight: 1 , description: 'The Nike Epic React Flyknit foam cushioning is responsive yet light-weight, durable yet soft. This creates a sensation that not only enhances the feeling of moving forward, but makes running feel fun, too.',label:"feature", stock: 1 }),
         db.products.create({id: 13,seller_id:2 ,name: 'Gucci belt', price: 400 , weight: 1 , description: 'The Nike Epic React Flyknit foam cushioning is responsive yet light-weight, durable yet soft. This creates a sensation that not only enhances the feeling of moving forward, but makes running feel fun, too.',label:"feature", stock: 1 }),
-        db.products.create({id: 14,seller_id:2 ,name: 'Rolex watch', price: 400 , weight: 1 , description: 'The Nike Epic React Flyknit foam cushioning is responsive yet light-weight, durable yet soft. This creates a sensation that not only enhances the feeling of moving forward, but makes running feel fun, too.',label:"sold", stock: 1 }),
-        db.products.create({id: 15,seller_id:2 ,name: 'Ring Pandora', price: 300 , weight: 1 , description: 'The Nike Epic React Flyknit foam cushioning is responsive yet light-weight, durable yet soft. This creates a sensation that not only enhances the feeling of moving forward, but makes running feel fun, too.',label:"sold", stock: 1 })
+        db.products.create({id: 14,seller_id:2 ,name: 'Rolex watch', price: 400 , weight: 1 , description: 'The Nike Epic React Flyknit foam cushioning is responsive yet light-weight, durable yet soft. This creates a sensation that not only enhances the feeling of moving forward, but makes running feel fun, too.',label:"feature", stock: 1 }),
+        db.products.create({id: 15,seller_id:2 ,name: 'Ring Pandora', price: 300 , weight: 1 , description: 'The Nike Epic React Flyknit foam cushioning is responsive yet light-weight, durable yet soft. This creates a sensation that not only enhances the feeling of moving forward, but makes running feel fun, too.',label:"classic", stock: 1 })
     ];
 
     const product_categoriesPromiseList = [
         db.product_categories.create({ productId:1,categoryId:1}),
         db.product_categories.create({ productId:1,categoryId:2}),
         db.product_categories.create({ productId:1,categoryId:3}),
-        db.product_categories.create({ productId:1,categoryId:7}),
         db.product_categories.create({ productId:1,categoryId:8}),
         db.product_categories.create({ productId:1,categoryId:9}),
 
         db.product_categories.create({ productId:6,categoryId:1}),
         db.product_categories.create({ productId:6,categoryId:2}),
         db.product_categories.create({ productId:6,categoryId:3}),
-        db.product_categories.create({ productId:6,categoryId:7}),
-        db.product_categories.create({ productId:6,categoryId:8}),
         db.product_categories.create({ productId:6,categoryId:9}),
 
-        db.product_categories.create({ productId:7,categoryId:1}),
-        db.product_categories.create({ productId:7,categoryId:2}),
-        db.product_categories.create({ productId:7,categoryId:3}),
-        db.product_categories.create({ productId:7,categoryId:7}),
-        db.product_categories.create({ productId:7,categoryId:8}),
-        db.product_categories.create({ productId:7,categoryId:9}),
+        db.product_categories.create({ productId:7,categoryId:5}),
+        db.product_categories.create({ productId:7,categoryId:17}),
         
-        db.product_categories.create({ productId:4,categoryId:1}),
-        db.product_categories.create({ productId:4,categoryId:2}),
-        db.product_categories.create({ productId:4,categoryId:3}),
-        db.product_categories.create({ productId:4,categoryId:7}),
-        db.product_categories.create({ productId:4,categoryId:8}),
-        db.product_categories.create({ productId:4,categoryId:9}),
+        db.product_categories.create({ productId:4,categoryId:6}),
+        db.product_categories.create({ productId:4,categoryId:14}),
 
 
-        db.product_categories.create({ productId:5,categoryId:1}),
-        db.product_categories.create({ productId:5,categoryId:2}),
-        db.product_categories.create({ productId:5,categoryId:3}),
-        db.product_categories.create({ productId:5,categoryId:7}),
-        db.product_categories.create({ productId:5,categoryId:8}),
+        db.product_categories.create({ productId:5,categoryId:6}),
+        db.product_categories.create({ productId:5,categoryId:13}),
 
-        db.product_categories.create({ productId:9,categoryId:1}),
-        db.product_categories.create({ productId:9,categoryId:2}),
-        db.product_categories.create({ productId:9,categoryId:3}),
-        db.product_categories.create({ productId:9,categoryId:7}),
-        db.product_categories.create({ productId:9,categoryId:8}),
+        db.product_categories.create({ productId:9,categoryId:20}),
+        db.product_categories.create({ productId:9,categoryId:21}),
 
         db.product_categories.create({ productId:12,categoryId:1}),
         db.product_categories.create({ productId:12,categoryId:2}),
         db.product_categories.create({ productId:12,categoryId:3}),
-        db.product_categories.create({ productId:12,categoryId:8}),
+        db.product_categories.create({ productId:12,categoryId:15}),
 
-        db.product_categories.create({ productId:10,categoryId:1}),
-        db.product_categories.create({ productId:10,categoryId:2}),
-        db.product_categories.create({ productId:10,categoryId:3}),
-        db.product_categories.create({ productId:10,categoryId:7}),
-        db.product_categories.create({ productId:10,categoryId:8}),
+        db.product_categories.create({ productId:8,categoryId:6}),
+        db.product_categories.create({ productId:8,categoryId:12}),
+        db.product_categories.create({ productId:8,categoryId:13}),
+
+        db.product_categories.create({ productId:10,categoryId:5}),
+        db.product_categories.create({ productId:10,categoryId:19}),
 
         db.product_categories.create({ productId:11,categoryId:1}),
         db.product_categories.create({ productId:11,categoryId:2}),
@@ -125,31 +111,33 @@ function dataInit() {
 
         db.product_categories.create({productId:3,categoryId:6}),
         db.product_categories.create({productId:3,categoryId:14}),
+        db.product_categories.create({productId:4,categoryId:6}),
+        db.product_categories.create({productId:4,categoryId:14}),
+        db.product_categories.create({productId:5,categoryId:6}),
+        db.product_categories.create({productId:5,categoryId:14}),
         
         db.product_categories.create({productId:2,categoryId:6}),
-        db.product_categories.create({productId:2,categoryId:14}),
+        db.product_categories.create({productId:2,categoryId:12}),
 
-        db.product_categories.create({productId:13,categoryId:6}),
-        db.product_categories.create({productId:13,categoryId:14}),
-        db.product_categories.create({productId:14,categoryId:6}),
-        db.product_categories.create({productId:14,categoryId:14}),
+        db.product_categories.create({productId:13,categoryId:2}),
+        db.product_categories.create({productId:13,categoryId:3}),
+        db.product_categories.create({productId:13,categoryId:4}),
+        db.product_categories.create({productId:13,categoryId:9}),
+
+        db.product_categories.create({productId:14,categoryId:1}),
+        db.product_categories.create({productId:14,categoryId:2}),
+        db.product_categories.create({productId:14,categoryId:3}),
+        db.product_categories.create({productId:14,categoryId:4}),
+        db.product_categories.create({productId:14,categoryId:11}),
 
         db.product_categories.create({productId:13,categoryId:15}),
         db.product_categories.create({productId:14,categoryId:15}),
 
-        db.product_categories.create({productId:8,categoryId:3}),
-        db.product_categories.create({productId:8,categoryId:4}),
-        db.product_categories.create({productId:8,categoryId:1}),
-        db.product_categories.create({productId:8,categoryId:2}),
-        db.product_categories.create({productId:8,categoryId:10}),
-        db.product_categories.create({productId:8,categoryId:11}),
 
+        db.product_categories.create({productId:15,categoryId:2}),
         db.product_categories.create({productId:15,categoryId:3}),
         db.product_categories.create({productId:15,categoryId:4}),
-        db.product_categories.create({productId:15,categoryId:1}),
-        db.product_categories.create({productId:15,categoryId:2}),
         db.product_categories.create({productId:15,categoryId:10}),
-        db.product_categories.create({productId:15,categoryId:11}),
     ];
 
     const product_imagesPromiseList = [
@@ -158,29 +146,43 @@ function dataInit() {
         db.product_images.create({product_id:1,image:'/images/shirt.jpg'}),
         db.product_images.create({product_id:2,image:'/images/iphone11.png'}),
         db.product_images.create({product_id:2,image:'/images/iphone11.png'}),
-        db.product_images.create({product_id:3,image:'/images/mba.jpg'}),
-        db.product_images.create({product_id:3,image:'/images/mba.jpg'}),
+
+        db.product_images.create({product_id:3,image:'/images/laptopHp.jpg'}),
+
         db.product_images.create({product_id:4,image:'/images/mba.jpg'}),
-        db.product_images.create({product_id:4,image:'/images/mba.jpg'}),
-        db.product_images.create({product_id:4,image:'/images/mba.jpg'}),
-        db.product_images.create({product_id:5,image:'/images/mba.jpg'}),
-        db.product_images.create({product_id:5,image:'/images/mba.jpg'}),
+
+        db.product_images.create({product_id:5,image:'/images/mac.jpg'}),
+
         db.product_images.create({product_id:6,image:'/images/tshirtBoss.jpg'}),
-        db.product_images.create({product_id:7,image:'/images/gucciBelt.jpg'}),
-        db.product_images.create({product_id:8,image:'/images/shoesNike.png'}),
-        db.product_images.create({product_id:9,image:'/images/shoesNike.png'}),
-        db.product_images.create({product_id:10,image:'/images/shoesNike.png'}),
+        
+        db.product_images.create({product_id:7,image:'/images/rostilj.jpg'}),
+        db.product_images.create({product_id:7,image:'/images/rostilj2.jpg'}),
+
+        db.product_images.create({product_id:8,image:'/images/ipadPro.jpg'}),
+
+        db.product_images.create({product_id:9,image:'/images/tesla.jpg'}),
+        db.product_images.create({product_id:9,image:'/images/tesla.jpg'}),
+        db.product_images.create({product_id:9,image:'/images/tesla2.jpg'}),
+
+        db.product_images.create({product_id:10,image:'/images/bedForTwo.jpg'}),
+
         db.product_images.create({product_id:11,image:'/images/shoesNike.png'}),
+
         db.product_images.create({product_id:12,image:'/images/bag.png'}),
+        db.product_images.create({product_id:13,image:'/images/gucciBelt.jpg'}),
+        db.product_images.create({product_id:13,image:'/images/gucciBelt.jpg'}),
         db.product_images.create({product_id:13,image:'/images/gucciBelt.jpg'}),
         db.product_images.create({product_id:14,image:'/images/rolexGold.jpg'}),
         db.product_images.create({product_id:14,image:'/images/rolexGreen.jpg'}),
-        db.product_images.create({product_id:15,image:'/images/ring.jpg'})
+        db.product_images.create({product_id:14,image:'/images/rolexGreen.jpg'}),
+        db.product_images.create({product_id:15,image:'/images/ring.jpg'}),
+        db.product_images.create({product_id:15,image:'/images/ring.jpg'}),
+        db.product_images.create({product_id:15,image:'/images/ring.jpg'}),
     ];
     
     const order_detailsPromiseList = [
-        db.order_details.create({id: 1, order_id:1 , product_id: 15 , price: 567 , quantity: 1 }),
-        db.order_details.create({id: 2, order_id:1 , product_id: 14 , price: 10000 , quantity: 1 })
+        //db.order_details.create({id: 1, order_id:1 , product_id: 15 , price: 567 , quantity: 1 }),
+        //db.order_details.create({id: 2, order_id:1 , product_id: 14 , price: 10000 , quantity: 1 })
     ];
     
     return new Promise((resolve, reject) => {
